91b258295d375cdabde9ce4cacbe53b7ed8e2228
Jonathan <johnbonilla007@gmail.com>
2024-12-19 08:52:18 -0600

    updating restclient 03

--- src/Helpers/restClient.js
+++ src/Helpers/restClient.js
@@ -13,7 +13,7 @@ export const getUrl = () => {
       return "https://dijoks3b3xwsy.cloudfront.net/api/v2";
 
     case "residencialquintasdelsol.netlify.app":
-      return "https://dxmwnutuoa2t2.cloudfront.net/api/v2";
+      return "https://dijoks3b3xwsy.cloudfront.net/api/v2";
 
     default:
       return "http://apissaweb.ssahonduras.com/api/v2";
