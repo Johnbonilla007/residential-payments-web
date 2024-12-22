84d399f6573ab41c0a6bcc9f49e9bcd8fba1c864
Jonathan <johnbonilla007@gmail.com>
2024-12-22 17:59:38 -0600

    changes

--- src/Container/Residentials/VisitsManager/index.js
+++ src/Container/Residentials/VisitsManager/index.js
@@ -2,34 +2,43 @@ import React, { useEffect, useRef, useState } from "react";
 import Container from "../../../Components/ContainerControl";
 import { VisitsManagerStyled } from "./styles";
 import { BrowserQRCodeReader } from "@zxing/browser";
+import { Toast } from "primereact/toast";
 
 const VisitsManager = () => {
   const videoRef = useRef(null);
+  const toast = useRef(null);
 
   useEffect(() => {
     const codeReader = new BrowserQRCodeReader();
-    codeReader.decodeFromVideoDevice(
+    codeReader?.decodeFromVideoDevice(
       null,
       videoRef.current,
       (result, error) => {
-        debugger;
         if (result) {
-          console.log(result.getText());
+          debugger;
+          const text = result.getText();
+          console.log(text);
+          toast.current.show({
+            severity: "warn",
+            summary: "Advertencia",
+            detail: text,
+            life: 3000,
+          });
         }
         if (error) {
-          debugger;
-
           console.error(error);
         }
       }
     );
 
     return () => {
-      codeReader.reset();
+      codeReader?.reset();
     };
   }, []);
   return (
     <Container>
+      <Toast ref={toast} />
+
       <VisitsManagerStyled>
         <video ref={videoRef} style={{ width: "100%" }} />
       </VisitsManagerStyled>
